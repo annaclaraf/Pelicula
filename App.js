88f9eac40ac6def9faa7a021455f0c174b64af08
Anna <anisferraz@gmail.com>
2022-09-11 14:53:27 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor="#A4BEE0" translucent={false} />
+      <LinearGradient colors={['#A4BEE0', '#161E29']} style={styles.background} />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%'
+  }
 });
