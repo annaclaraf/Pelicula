2a6fb4edd2463f6f21d7779acf95bc6fbefe8cc7
Anna <anisferraz@gmail.com>
2022-09-20 06:38:27 -0300

    remove linear gradient background color

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#A4BEE0" translucent={false} />
-      <LinearGradient colors={['#A4BEE0', '#161E29']} style={styles.background} />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -14,13 +12,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  background: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: '100%'
+    flex: 1,
+    backgroundColor: '#D5E1F1'
   }
 });
