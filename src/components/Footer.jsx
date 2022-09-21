f4183a7b1f34c07faad9082d59d99f821fcef505
Anna <anisferraz@gmail.com>
2022-09-21 06:14:49 -0300

    add: navigation between screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native'
 import { View, Alert, Pressable, StyleSheet } from 'react-native';
 import { House, MagnifyingGlass, User } from 'phosphor-react-native'
 
 export function Footer() {
+  const navigation = useNavigation()
+
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => { Alert.alert('Test', 'Home') }}>
+      <Pressable onPress={() => navigation.navigate('home')}>
         <House color="#FFFFFF" size={26} />
       </Pressable>
-      <Pressable onPress={() => { Alert.alert('Test', 'Search') }}>
+      <Pressable onPress={() => navigation.navigate('search')}>
         <MagnifyingGlass color="#FFFFFF" size={26} />
       </Pressable>
       <Pressable onPress={() => { Alert.alert('Test', 'Profile') }}>
