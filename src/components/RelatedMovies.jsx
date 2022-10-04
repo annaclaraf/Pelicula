5f931b73bf76310e259fe1bf2408699fbeb521ec
Anna <anisferraz@gmail.com>
2022-10-04 19:47:57 -0300

    feat: related movies component

---
+++ src/components/RelatedMovies.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { MovieSlider } from './MovieSlider'
+
+export function RelatedMovies({ movieId }) {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=17830acb428fca194205745d95c40ae4&language=pt-BR`, { method: "GET" })
+        .then((res) => res.json())
+        .then((res) => res.results)
+
+      if (response.length == 0) {
+        response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=17830acb428fca194205745d95c40ae4&language=pt-BR`, { method: "GET" })
+          .then((res) => res.json())
+          .then((res) => res.results)
+      }
+
+      setData(response)
+    })();
+  }, [])
+
+  return (
+    <View>
+      <Text style={styles.title}>Filmes Similares</Text>
+      <MovieSlider data={data} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: '700',
+    fontSize: 20,
+    color: '#0A1823',
+    paddingVertical: 10
+  }
+});
