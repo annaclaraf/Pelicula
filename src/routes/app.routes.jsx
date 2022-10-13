98d42011f67209fd0cf58d4bf71700e3ba62b425
Anna <anisferraz@gmail.com>
2022-10-13 16:10:46 -0300

    feat: list all movies screen route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,6 +4,7 @@ import { Search } from '../screens/Search'
 import { Home } from '../screens/Home'
 import { Details } from '../screens/Details'
 import { Profile } from '../screens/Profile'
+import { ListAllMovies } from '../screens/ListAllMovies'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -14,6 +15,7 @@ export function AppRoutes() {
       <Screen name="search" component={Search} />
       <Screen name="details" component={Details} />
       <Screen name="profile" component={Profile} />
+      <Screen name="listMovies" component={ListAllMovies} />
     </Navigator>
   )
 }
