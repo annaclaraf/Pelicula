fd4f31865c1b1f1a4574d01a28a85a4850e1afb5
Anna <anisferraz@gmail.com>
2022-10-13 15:49:24 -0300

    feat: profile route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Search } from '../screens/Search'
 import { Home } from '../screens/Home'
 import { Details } from '../screens/Details'
+import { Profile } from '../screens/Profile'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -12,6 +13,7 @@ export function AppRoutes() {
       <Screen name="home" component={Home} />
       <Screen name="search" component={Search} />
       <Screen name="details" component={Details} />
+      <Screen name="profile" component={Profile} />
     </Navigator>
   )
 }
