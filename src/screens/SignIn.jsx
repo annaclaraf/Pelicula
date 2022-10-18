75651f567591d3005858a617de8c72f5ff28e8ed
Anna <anisferraz@gmail.com>
2022-10-18 15:36:33 -0300

    feat: sign in screen

---
+++ src/screens/SignIn.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { GoogleLogo } from 'phosphor-react-native'
+
+export function SignIn() {
+
+  return (
+    <View style={styles.wrapper}>
+      <Image style={styles.logo} source={{ uri: 'https://cdn.discordapp.com/attachments/901295847793360909/1031866236126105671/logo2.png' }} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Faça login com sua conta do Google</Text>
+        <TouchableOpacity style={styles.btnGoogle}>
+          <GoogleLogo weight='bold' color='#FFF' size={26} />
+          <Text style={styles.btnText}>
+            Entrar com Google
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#F2F2F2',
+    alignItems: "center",
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    width: '85%',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 350,
+    height: 60,
+    position: 'absolute',
+    top: 100
+  },
+  text: { 
+    fontSize: 20, 
+    fontWeight: 'bold', 
+    color: 'gray', 
+    marginBottom: 15 
+  },
+  btnGoogle: { 
+    width: '100%',
+    height: 50, 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    backgroundColor: '#DB4A39', 
+    borderRadius: 5
+  },
+  btnText: { 
+    color: '#FFF', 
+    fontSize: 16, 
+    fontWeight: 'bold',
+    marginLeft: 10
+  }
+})
