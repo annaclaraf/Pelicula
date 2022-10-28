851fca61dce244074743d56f5a218a7f9a23d1af
Anna <anisferraz@gmail.com>
2022-10-28 15:41:27 -0300

    add: auth provider

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
+import { AuthProvider } from './src/hooks/useAuth' 
 import { Routes } from './src/routes' 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" translucent={false} />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </View>
   ); 
 }
