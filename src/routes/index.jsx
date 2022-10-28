7b6cb99e9c4ed86207e6127b2e83905b19789040
Anna <anisferraz@gmail.com>
2022-10-28 15:40:08 -0300

    update: routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
 
+import { useAuth } from '../hooks/useAuth'
+
 import { AppRoutes } from './app.routes'
+import { SignIn } from '../screens/SignIn'
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user.id ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   )
-
 }
