41fbea48d321ef0deea938225f1c12afdd9397d7
Anna <anisferraz@gmail.com>
2022-12-01 22:18:25 -0300

    sort movies by register date

--- src/utils/getAllMovies.js
+++ src/utils/getAllMovies.js
@@ -3,7 +3,7 @@ import { moviesToWatchApi, moviesWatchedApi } from '../services/api'
 export async function AllMoviesToWatch(user) {
   const movies = await moviesToWatchApi.get(`/movies/watch/${user.id}`)
     .then(res => res.data)
-    .then(res => res.sort((a, b) => a.registerDate - b.registerDate));
+    .then(res => res.sort((a, b) => new Date(b.registerDate) - new Date(a.registerDate)));
 
   return movies;
 }
@@ -11,7 +11,7 @@ export async function AllMoviesToWatch(user) {
 export async function AllMoviesWatched(user){
   const movies = await moviesWatchedApi.get(`/movies/watched/${user.id}`)
     .then(res => res.data)
-    .then(res => res.sort((a, b) => a.registerDate - b.registerDate));
+    .then(res => res.sort((a, b) => new Date(b.registerDate) - new Date(a.registerDate)));
 
   return movies;
 }
